abc537c8625e64694dd1b5d69b01125e2d4f592d
Yu Zhanbei <awesome.zhanbei@gmail.com>
2018-11-03 13:09:29 +0800

    [src/helpers] Added #ConsoleLogger as the default logger.

---
+++ src/helpers/ConsoleLogger.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import newConsoleLogger from '../utils/ConsoleLogger';
+
+// TO-DO Make the #isDebugging configurable in the options page.
+export const isDebugging = false;
+export const ConsoleLogger = newConsoleLogger();
+export default ConsoleLogger;
+
+// Init the default logger.
+if (isDebugging) {
+	ConsoleLogger.setMinLoggingLevel(ConsoleLogger.LoggerVerbose.level);
+} else {
+	ConsoleLogger.setMinLoggingLevel(ConsoleLogger.LoggerWarning.level);
+}
